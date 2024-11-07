3c89519aee598ce7d394b0808c6a04a95713d81a
zahoorshinwari <khanzshinwari5371@gmail.com>
2024-11-08 00:17:11 +0500

    backend api uploaded

---
+++ index.js
@@ -0,0 +1,31 @@
+// index.js
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { UserRouter } = require('./routes/user');
+const { AdminRouter } = require('./routes/admin');
+const { connectDB } = require('./config/db');
+const { CompanyRouter } = require('./routes/company');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: ["http://localhost:5173"], // Adjust as needed for your frontend
+  credentials: true
+}));
+app.use(cookieParser());
+app.use('/api', UserRouter);
+
+app.use('/api/admin', AdminRouter); // Prefix the admin routes with /api/admin
+
+app.use('/api/', CompanyRouter)
+
+// Connect to the database and start the server
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  });
+});
